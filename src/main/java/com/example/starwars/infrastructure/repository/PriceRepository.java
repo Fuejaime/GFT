caf780d7108c37149f28810d4fcc17f62ca2f197
jaimegomez <jaimegfuenmayor@gmail.com>
2025-03-10 21:27:20 +0100

    Fix query to acceptance criteria

--- src/main/java/com/example/starwars/infrastructure/repository/PriceRepository.java
+++ src/main/java/com/example/starwars/infrastructure/repository/PriceRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
     @Repository
     public interface PriceRepository extends JpaRepository<PriceEntity, Long> {
         @Query("SELECT p FROM PriceEntity p WHERE p.productId = :productId AND p.brand.id = :brandId " +
-                "AND p.startDate <= :date AND p.endDate >= :date")
+                "AND p.startDate <= :date AND p.endDate >= :date ORDER BY p.priority DESC LIMIT 1")
         List<PriceEntity> findByProductIdAndBrandIdAndDate(
                 @Param("productId") int productId,
                 @Param("brandId") Long brandId, // Ahora es un Long, ya que es la PK de BrandEntity
